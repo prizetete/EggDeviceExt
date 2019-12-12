537788235f669ec2b905a7bd3b7af5ca9d2a0df7
KomkritSir <komkrit.sir@ascendcorp.com>
2019-12-12 13:39:57 +0700

    0.1.5

---
+++ EggDeviceExt/Classes/UIFontExt.swift
@@ -0,0 +1,14 @@
+//
+//  UIFontExt.swift
+//  EggDeviceExt
+//
+//  Created by Komkrit Siratitanan on 12/12/2562 BE.
+//
+
+import Foundation
+
+extension UIFont {
+    public func setFontSystem25() {
+        
+    }
+}
